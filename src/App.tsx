6911c775a4e68eeb6268bec54c8b885824dd3fca
mukuru1 <kabengaedwin@gmail.com>
2026-01-29 13:37:40 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,5 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
-import Causes from "@/pages/Causes";
-import Contact from "@/pages/Contact";
-import About from "@/pages/About";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/causes" component={Causes} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/about" component={About} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+  return <></>
 }
 
-export default App;
+export default App
